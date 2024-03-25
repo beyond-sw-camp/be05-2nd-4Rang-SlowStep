601edd04261acfadc86b44db6b5996eafc458c39
Bakhyegyeong <calla3494qhk@naver.com>
2024-03-25 17:55:55 +0900

    add getExamInfoNM

--- src/main/java/com/example/slowstep_pjt/managePatient/mapper/MPNurseMapper.java
+++ src/main/java/com/example/slowstep_pjt/managePatient/mapper/MPNurseMapper.java
@@ -2,10 +2,12 @@ package com.example.slowstep_pjt.managePatient.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.slowstep_pjt.managePatient.domain.EXAM_INFO;
+
 @Mapper
 public interface MPNurseMapper {
 
-
+    public EXAM_INFO getExamInfoNM(Integer examNo);
     
 }
 
--- src/main/java/com/example/slowstep_pjt/managePatient/service/MPNurseService.java
+++ src/main/java/com/example/slowstep_pjt/managePatient/service/MPNurseService.java
@@ -1,9 +1,12 @@
 package com.example.slowstep_pjt.managePatient.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.slowstep_pjt.managePatient.domain.EXAM_INFO;
 import com.example.slowstep_pjt.managePatient.mapper.MPNurseMapper;
 
 
@@ -12,6 +15,11 @@ public class MPNurseService {
 
     @Autowired
     private MPNurseMapper mpNurseMapper;
+
+    public EXAM_INFO getExamInfoNM(Integer examNo) {
+        
+        return mpNurseMapper.getExamInfoNM(examNo);
+    }
     
 
 }
