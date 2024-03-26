41d975118d1bebb4ef45a1d7385088dcac42f540
peterparker <ericj1204@gmail.com>
2024-03-26 16:42:06 +0900

    Add user information modifications

--- src/main/java/com/example/slowstep_pjt/user/mapper/UserMapper.java
+++ src/main/java/com/example/slowstep_pjt/user/mapper/UserMapper.java
@@ -1,19 +1,17 @@
 package com.example.slowstep_pjt.user.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.slowstep_pjt.user.domain.UserDTO;
 
-
-
 @Mapper
 public interface UserMapper {
+    // 기존의 로그인 메소드
+    UserDTO loginRow(String mbrEml);
 
-    public UserDTO loginRow(String mbrEml) ;
-
-   
-
-    
-    
+    // 사용자 정보를 업데이트하는 메소드 추가
+  
+    void updateUser(UserDTO userDTO);
 }
